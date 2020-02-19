f8e62415f26ee82e29db158fc0fbb8e592555405
lscpku <44794370+lscpku@users.noreply.github.com>
2020-02-19 12:23:54 +0800

    update

--- src/visit.js
+++ src/visit.js
@@ -2,12 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var i;
-            var xmlDoc = this.responseXML;
-            var x = xmlDoc.getElementsByTagName("counter");
-            console.log(x[0].childNodes[0].nodeValue);
+            console.log(this.responseText);
         }
     };
-    xhttp.open("GET", "src/counter.xml", true);
+    xhttp.open("GET", "src/visit.php", true);
     xhttp.send();
 })
