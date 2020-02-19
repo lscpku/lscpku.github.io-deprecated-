990dee3cdc5c7527455d8c50fe0e5e0dd7d096c8
lscpku <44794370+lscpku@users.noreply.github.com>
2020-02-19 12:48:22 +0800

    Update visit.php

--- src/visit.php
+++ src/visit.php
@@ -1,14 +1,14 @@
-<? php
+<?php
     $file_name = 'src/counter.xml';
     $xml = simplexml_load_file(file_name);
     $num = (int)($xml->visitor)+1;
     $dom = new DOMDocument();
-        $dom->encoding = 'utf-8';
-        $dom->xmlVersion = '1.0';
-        $dom->formatOutput = true;
-        $root = $dom->createElement('content');
-        $visitor = $dom->createElement('visitor', $num);
-        $dom->appendChild($root);
-        $root->appendChild($visitor);
+    $dom->encoding = 'utf-8';
+    $dom->xmlVersion = '1.0';
+    $dom->formatOutput = true;
+    $root = $dom->createElement('content');
+    $visitor = $dom->createElement('visitor', $num);
+    $dom->appendChild($root);
+    $root->appendChild($visitor);
     echo $num;
 ?>
